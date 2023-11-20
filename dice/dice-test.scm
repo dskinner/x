e003065b22cc93db70a63b914a4c81bf7a248c3a
Daniel Skinner <daniel@dasa.cc>
2023-11-19 22:11:41 -0600

    scheme meet dice

---
+++ dice/dice-test.scm
@@ -0,0 +1,149 @@
+(use-modules (ice-9 format)
+             (ice-9 match)
+             (srfi srfi-43)
+             (srfi srfi-64)
+             (fibers)
+             (fibers channels)
+             (dice))
+
+(test-begin "tests")
+
+;; all vector elements are unique
+(let ((v (gen 3 4)))
+  (vector-for-each
+   (lambda (_ a)
+     (test-eq 1 (vector-count (lambda (_ b) (equal? b a)) v)))
+   v))
+
+;; action equality
+(let ((a (make-action 1 3))
+      (b (make-action 1 3))
+      (c (make-action 2 3)))
+  (test-assert (equal? a b))
+  (test-assert (not (equal? a c))))
+
+;; by action speed, then power
+(let ((a (make-action 6 2))
+      (b (make-action 6 3))
+      (c (make-action 1 4))
+      (d (make-action 2 4))
+      (e (make-action 2 4)))
+  (test-assert (less-speed-power a b))
+  (test-assert ((negate less-speed-power) b a))
+  
+  (test-assert (less-speed-power b c))
+  (test-assert ((negate less-speed-power) c b))
+  
+  (test-assert (less-speed-power c d))
+  (test-assert ((negate less-speed-power) d c))
+  
+  (test-assert ((negate less-speed-power) d e))
+  (test-assert ((negate less-speed-power) e d)))
+
+;; action-list-penalize
+;; TODO
+
+;; conflict resolution
+(let ((a '())
+      (b '()))
+  (test-equal (values 0 0) (conflict-resolve a 0 b 0)))
+
+(let ((a (list (make-action 6 2)))
+      (b '()))
+  (test-equal (values 1 0) (conflict-resolve a 0 b 0)))
+
+(let ((a '())
+      (b (list (make-action 6 2))))
+  (test-equal (values 0 1) (conflict-resolve a 0 b 0)))
+
+(let ((a (list (make-action 6 2)))
+      (b (list (make-action 6 2))))
+  (test-equal (values 0 0) (conflict-resolve a 0 b 0)))
+
+(let ((a (list (make-action 5 2)))
+      (b (list (make-action 6 2))))
+  (test-equal (values 0 1) (conflict-resolve a 0 b 0)))
+
+(let ((a (list (make-action 6 2)))
+      (b (list (make-action 5 2))))
+  (test-equal (values 1 0) (conflict-resolve a 0 b 0)))
+
+(let ((a (list (make-action 5 3)))
+      (b (list (make-action 6 2))))
+  (test-equal (values 1 0) (conflict-resolve a 0 b 0)))
+
+(let ((a (list (make-action 5 3)))
+      (b (list (make-action 6 3))))
+  (test-equal (values 1 1) (conflict-resolve a 0 b 0)))
+
+(let ((a (gen-actions-valid-sorted 3 6))
+      (b (gen-actions-valid-sorted 4 6)))
+  (test-assert (display (format #f "3v3d6 ~a\n" (result->string (resolve-all a a)))))
+  (test-assert (display (format #f "3v4d6 ~a\n" (result->string (resolve-all a b)))))
+  (test-assert (display (format #f "4v4d6 ~a\n" (result->string (resolve-all b b))))))
+
+;; test async
+
+(run-fibers
+ (lambda () 
+   (let* ((a (gen-actions-valid-sorted 3 6))
+          (b (gen-actions-valid-sorted 4 6))
+          (r (resolve-all a b))
+          (s (resolve-all-fibers a b)))
+     (test-assert (equal? r s))))
+ #:drain? #t)
+
+(test-end "tests")
+
+(define-syntax-rule (benchmark e ...)
+  (let ((now (tms:clock (times))))
+    (begin e ...)
+    (display (format #f "~,2fs ~a\n" (/ (- (tms:clock (times)) now) 1e9) (quote e ...)))))
+
+(define (-bench)
+  ;; 0.05s
+  (benchmark (gen 6 6))
+  ;; 0.12s
+  (benchmark (gen-actions 6 6))
+  ;; 0.18s
+  (benchmark (gen-actions-valid-sorted 6 6))
+  ;; 0.28s
+  (benchmark (resolve-all (gen-actions-valid-sorted 4 6) (gen-actions-valid-sorted 4 6)))
+
+  ;; 2.01s
+  (benchmark (resolve-all (gen-actions-valid-sorted 4 6) (gen-actions-valid-sorted 5 6)))
+  ;; 0.65s
+  (benchmark (resolve-all-fibers (gen-actions-valid-sorted 4 6) (gen-actions-valid-sorted 5 6)))
+
+  ;; 16.90s
+  ;; (benchmark (resolve-all (gen-actions-valid-sorted 5 6) (gen-actions-valid-sorted 5 6)))
+  ;; 5.52s
+  (benchmark (resolve-all-fibers (gen-actions-valid-sorted 5 6) (gen-actions-valid-sorted 5 6)))
+  
+  )
+
+(define (bench args)
+  (display (format #f "%%%% Starting benchmarks ~a\n" args))
+  (run-fibers -bench #:drain? #t))
+
+(define (plot-vs n0 n1)
+  ;; sorting by wins illustrates the small number of groups all rolls will fall into
+  ;; TODO check if tie,loss,part are all equal within each group
+  (let* ((a (gen-actions-valid-sorted n0 6))
+         (b (gen-actions-valid-sorted n1 6))
+         (rs (list->vector (sort (resolve-each-fibers a b) less-win))))
+    (with-output-to-file (format #f "n~av~ad6.dat" n0 n1)
+      (lambda ()
+        (vector-for-each
+         (lambda (i r)
+           (display (format #f "~f ~f\n" (/ (+ 1 i) (vector-length rs)) (result-win-percent r))))
+         rs)))))
+
+(define (-plot)
+  (plot-vs 4 3)
+  (plot-vs 4 4)
+  (plot-vs 4 5))
+
+(define (plot args)
+  (display (format #f "%%%% Starting plots ~a\n" args))
+  (run-fibers -plot #:drain? #t))
---
+++ dice/dice.scm
@@ -0,0 +1,206 @@
+(define-module (dice)
+  #:export (action action? make-action action-speed action-power))
+
+(use-modules (ice-9 exceptions)
+             (ice-9 format)
+             (ice-9 receive)
+             (fibers)
+             (fibers channels)
+             (srfi srfi-1)
+             (srfi srfi-9)
+             (srfi srfi-43))
+
+;; returns sample space for rolls of given dice set nDs. The table is sized
+;; as columns of length n and rows of length s^n. Notating each column from
+;; s^0..s^n-1, this is the number of times a column value is repeatedly assigned
+;; before incrementing to the next value.
+;;
+;; For example, column zero of 3d4 would be notated 4^0 which equals 1. So column
+;; values would go 1, 2, 3, 4, 1, 2, 3, 4, 1, 2 and so on.
+;;
+;; Column one would be notated 4^1 which equals 4. Column values would go
+;; 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, and so on.
+;;
+;; Column two of 3d4 would be notated 4^2 which equals 16. The first sixteen column
+;; values would be 1, the next sixteen would be 2, and so on.
+(define-public (gen n s)
+  ;; precompute column span sizes
+  (define sizes (vector-unfold (lambda (j) (expt s j)) n))
+  
+  (vector-unfold
+   (lambda (i)
+     (vector-unfold
+      (lambda (j)
+        (+ 1 (modulo (truncate (/ i (vector-ref sizes j))) s)))
+      n))
+   (expt s n)))
+
+;; convenience method returning all actions for sample space nDs.
+(define-public (gen-actions n s)
+  (vector->list
+   (vector-map (lambda (_ x) (vector->actions x s)) (gen n s))))
+
+;; convenience method returning all valid actions sorted for sample space nDs.
+(define-public (gen-actions-valid-sorted n s)
+  (filter (negate null-list?) ;; some rolls have no valid actions
+          (map (lambda (a) (sort (filter action-valid? a) (negate less-speed-power)))
+               (gen-actions n s))))
+
+;; action is the core game mechanic defined by grouping dice of the same face-value.
+;; There must be at least two of the same face-value to be considered a valid action.
+(define-record-type action
+  (make-action power speed)
+  action?
+  (power action-power)  ;; face-value of dice roll
+  (speed action-speed)) ;; number of dice for given face-value
+
+;; predicate identifying if at least two dice rolled the same face-value.
+(define-public (action-valid? a) (<= 2 (action-speed a)))
+
+;; an action penalty removes a die from the set, potentially making the action no longer valid.
+(define-public (action-with-penalty a)
+  (make-action (action-power a) (- (action-speed a) 1)))
+
+;; while conflict resolution has many possibilities, this implementation assumes the desire to penalize
+;; the action with the highest power and speed; this may not necessarily be the best move.
+(define-public (action-list-penalize l)
+  (sort (filter action-valid? (cons (action-with-penalty (car l)) (cdr l))) (negate less-speed-power)))
+
+;; transforms a vector of dice rolls into a list of actions, e.g. 5d6:
+;;   (vector->actions #(1 1 1 4 4) 6)
+;;   (list #<action power: 1 speed: 3> #<action power: 4 speed: 2> ...)
+(define-public (vector->actions v s)
+  (vector->list ;; TODO consider srfi-1 for list (unfold ...)
+   (vector-unfold (lambda (i) (make-action (+ 1 i) (vector-count (lambda (_ x) (= x (+ 1 i))) v))) s)))
+
+;; less by speed, then power
+(define-public (less-speed-power a b)
+  (or 
+   (< (action-speed a) (action-speed b))
+   (and
+    (= (action-speed a) (action-speed b))
+    (< (action-power a) (action-power b)))))
+
+;; resolve a against b with seeds x and y; a and b must be sorted and valid action lists.
+(define-public (conflict-resolve a x b y)
+  (cond ((and (nil? a) (nil? b)) (values x y))
+        ((nil? a) (conflict-resolve a x (cdr b) (+ 1 y)))
+        ((nil? b) (conflict-resolve (cdr a) (+ 1 x) b y))
+        ((equal? (car a) (car b)) (conflict-resolve (cdr a) x (cdr b) y))
+        ((less-speed-power (car a) (car b)) (conflict-resolve (action-list-penalize a) x (cdr b) (+ 1 y)))
+        ((less-speed-power (car b) (car a)) (conflict-resolve (cdr a) (+ 1 x) (action-list-penalize b) y))
+        (else (raise-exception (make-exception-with-message "unreachable case"))))) ;; hopefully
+
+;; analyze conflict result to infer win, loss, tie, partial.
+;; (define-public (conflict-analyze x y) ;; actions resulted in ...
+;;   (cond ((and (= 0 x) (= 0 y)) 'tie)  ;; no penalties
+;;         ((and (< 0 x) (= 0 y)) 'win)  ;; penalties against y
+;;         ((and (= 0 x) (< 0 y)) 'loss) ;; penalties against x
+;;         ((= 0 (- x y)) 'part) ;; equal penalties against x and y
+;;         ((< 0 (- x y)) 'win)  ;; penalties favoring x over y
+;;         (else 'loss)))        ;; penalties favoring y over x
+
+;; convenience to resolve and analyze a conflict.
+;; (define-public (conflict-resolve-and-analyze a b)
+;;   (call-with-values (lambda () (conflict-resolve a 0 b 0)) conflict-analyze))
+
+(define-record-type result
+  (make-result win tie loss part)
+  result?
+  (win result-win set-result-win!)
+  (tie result-tie set-result-tie!)
+  (loss result-loss set-result-loss!)
+  (part result-part set-result-part!))
+
+;; analyze and record action results from the perspective of x.
+(define-public (result-analyze! r x y)
+  (cond ((and (= 0 x) (= 0 y)) (set-result-tie! r (+ 1 (result-tie r)))) ;; no penalties
+        ((and (< 0 x) (= 0 y)) (set-result-win! r (+ 1 (result-win r)))) ;; penalties against y
+        ((and (= 0 x) (< 0 y)) (set-result-loss! r (+ 1 (result-loss r)))) ;; penalties against x
+        ((= 0 (- x y)) (set-result-part! r (+ 1 (result-part r)))) ;; equal penalties against x and y
+        ((< 0 (- x y)) (set-result-win! r (+ 1 (result-win r)))) ;; penalties favoring x over y
+        (else (set-result-loss! r (+ 1 (result-loss r))))))      ;; penalties favoring y over x
+
+;; additive merge s into r.
+(define-public (result-merge! r s)
+  (set-result-win! r (+ (result-win r) (result-win s)))
+  (set-result-tie! r (+ (result-tie r) (result-tie s)))
+  (set-result-loss! r (+ (result-loss r) (result-loss s)))
+  (set-result-part! r (+ (result-part r) (result-part s))))
+
+(define-public (result-percent-coef r) (/ 100 (+ (result-win r) (result-tie r) (result-loss r) (result-part r))))
+(define-public (result-win-percent r) (* (result-percent-coef r) (result-win r)))
+(define-public (result->string r)
+  (let ((t (result-percent-coef r)))
+    (format #f "win:~,2f% tie:~,2f% loss:~,2f% part:~,2f%"
+            (* t (result-win r))
+            (* t (result-tie r))
+            (* t (result-loss r))
+            (* t (result-part r)))))
+
+(define-public (less-win a b) (< (result-win a) (result-win b)))
+
+;; resolve conflicts and accumulate result for dice set.
+(define-public (resolve-all as bs)
+  (let ((r (make-result 0 0 0 0)))
+    (for-each
+     (lambda (a)
+       (for-each
+        (lambda (b)
+          (receive (x y)
+              (conflict-resolve a 0 b 0)
+            (result-analyze! r x y)))
+        bs))
+     as)
+    r))
+
+(define-public (resolve-all-fibers as bs)
+  (let ((rs (make-channel)))
+    (for-each
+     (lambda (a)
+       (spawn-fiber
+        (lambda ()
+          (put-message rs (resolve-action-list a bs)))
+        #:parallel? #t))
+     as)
+
+    (let ((r (make-result 0 0 0 0)))
+      (let lp ((n (- (length as) 1)))
+        (result-merge! r (get-message rs))
+        (unless (= 0 n) (lp (- n 1))))
+      r)))
+
+;; resolve conflicts and accumulate results for individual action sets of dice set.
+(define-public (resolve-each as bs)
+  (let ((rs '()))
+    (for-each
+     (lambda (a)
+       (set! rs (append rs (list (resolve-action-list a bs)))))
+     as)
+    rs))
+
+(define-public (resolve-each-fibers as bs)
+  (let ((chan (make-channel)))
+    (for-each
+     (lambda (a)
+       (spawn-fiber
+        (lambda ()
+          (put-message chan (resolve-action-list a bs)))
+        #:parallel? #t))
+     as)
+
+    (let ((rs '()))
+      (let lp ((n (- (length as) 1)))
+        (set! rs (append rs (list (get-message chan))))
+        (unless (= 0 n) (lp (- n 1))))
+      rs)))
+
+(define (resolve-action-list a bs)
+  (let ((r (make-result 0 0 0 0)))
+    (for-each
+     (lambda (b)
+       (receive (x y)
+           (conflict-resolve a 0 b 0)
+         (result-analyze! r x y)))
+     bs)
+    r))
